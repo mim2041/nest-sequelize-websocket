783891218bb8f5c3d2c4dc72b263ce3a3d110a94
mim2041 <suraiyamim338@gmail.com>
2024-09-19 23:29:16 +0600

    guards added to prevent duplicate email signup

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,19 +1,21 @@
-import { Body, Controller, Post, Request, UseGuards } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthGuard } from "@nestjs/passport";
 import { UserDto } from "../users/dto/user.dto";
+import { DoesUserExist } from "src/core/guards/doesUserExist.guard";
 
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) { }
 
-    // @UseGuards(AuthGuard('local'))
+    @UseGuards(AuthGuard('local'))
     @Post('login')
     async login(@Body() user: any) {
         return this.authService.login(user);
     }
 
+    @UseGuards(DoesUserExist)
     @Post('signup')
     async signUp(@Body() user: UserDto) {
         return this.authService.create(user);
--- src/modules/users/dto/user.dto.ts
+++ src/modules/users/dto/user.dto.ts
@@ -1,10 +1,30 @@
+import { IsEmail, IsEnum, IsNotEmpty, MinLength } from "class-validator";
+
+
+
+enum role {
+    CUSTOMER = 'customer',
+    DELIVERY_PERSON = 'delivery_person',
+}
+
 export class UserDto {
-    id: number;
-    firstname: string;
-    lastname: string;
-    email: string;
-    password: string;
-    role: string;
-    createdAt: Date;
-    updatedAt: Date;
+    @IsNotEmpty()
+    readonly firstname: string;
+
+    @IsNotEmpty()
+    readonly lastname: string;
+
+    @IsNotEmpty()
+    @IsEmail()
+    readonly email: string;
+
+    @IsNotEmpty()
+    @MinLength(6)
+    readonly password: string;
+
+    @IsNotEmpty()
+    @IsEnum(role, {
+        message: `role must be either customer or delivery_person`,
+    })
+    readonly role: role;
 }
